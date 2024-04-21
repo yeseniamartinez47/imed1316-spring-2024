39a7c7d29800d83c2d58ad83d9fc5e78bd799fe2
yeseniamartinez47 <157070736+yeseniamartinez47@users.noreply.github.com>
2024-04-21 13:40:17 -0500

    updates

--- js/gallery.js
+++ js/gallery.js
@@ -3,9 +3,19 @@ $(document).ready(function () {
     //console.log( "ready!" );
 
     $("#blinn-gallery").nanogallery2({
-        ...
-        thumbnailHeight: 200,
-        thumbnailWidth: 200
+        thumbnailWidth: "300",
+        thumbnailHeight: "auto",
+        thumbnailBorderVertical: 0,
+        thumbnailBorderHorizontal: 0,
+        thumbnailLabel: {
+            position: "overImageOnBottom",
+            display: false
+        },
+        thumbnailHoverEffect2: "imageScaleIn80|imageSepiaOff|labelAppear75",
+        thumbnailGutterWidth: 0,
+        thumbnailGutterHeight: 0,
+        thumbnailAlignment: "center",
+        thumbnailOpenImage: true
     });
 
 });
