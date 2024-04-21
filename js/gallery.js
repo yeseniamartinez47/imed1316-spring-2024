6335431a8c9396936eb2c8e6448e7c855b4e538b
yeseniamartinez47 <157070736+yeseniamartinez47@users.noreply.github.com>
2024-04-21 13:29:08 -0500

    added tags

---
+++ js/gallery.js
@@ -0,0 +1,11 @@
+// A $( document ).ready() block.
+$(document).ready(function () {
+    //console.log( "ready!" );
+
+    $("#blinn-gallery").nanogallery2({
+        ...
+        thumbnailHeight: 200,
+        thumbnailWidth: 200
+    });
+
+});
