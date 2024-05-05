8ea68d1c7b1f0e39ba425e938f72858d87241982
yeseniamartinez47 <157070736+yeseniamartinez47@users.noreply.github.com>
2024-05-05 17:35:36 -0500

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,38 @@
+// A $( document ).ready() block.
+$(document).ready(function () {
+    //console.log("ready!");
+
+    $(".slider-for").slick({
+        centerMode: true,
+        centerPadding: "60px",
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: "40px",
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: "40px",
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+}); //document ready
+
+
+
+
+
 // init Isotope
 var $grid = $(".grid").isotope({
     itemSelector: ".element-item",
